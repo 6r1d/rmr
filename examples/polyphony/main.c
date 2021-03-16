651ffb6d3fc895e5ec1a956ea8888034e2a40e35
Victor Gridnevsky <vic.6r1d@gmail.com>
2021-03-16 20:51:21 +0300

    Solves the latency issue

--- examples/polyphony/main.c
+++ examples/polyphony/main.c
@@ -1,19 +1,28 @@
+/*
+ * An polyphony example to play around.
+ * Based on Acratone's polyphony and Libsoundio sine example.
+ * https://github.com/Saegor/acratone/blob/master/sound.c
+ * https://github.com/andrewrk/libsoundio/blob/master/example/sio_sine.c
+ *
+ * Run like:
+ * ./main --latency 0.1
+ * or even
+ * ./main --latency 0.01
+ */
+
+
 #include <soundio/soundio.h>
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
+#include <stdint.h>
 #include <math.h>
-#include <assert.h>
-
-#include <stdlib.h>
+#include "sample.h"
 
 #include "midi/midi_handling.h"
 
 #include "poly_gen.h"
-
-unsigned int r = 0;
 
 Alsa_MIDI_data * data;
 MIDI_in_data * input_data;
@@ -52,17 +61,30 @@ void handle_midi_buffer(unsigned char * buf, long count) {
 }
 
 void scan_queue() {
-        while (g_async_queue_length_unlocked(input_data->midi_async_queue)) {
+        while (g_async_queue_length(input_data->midi_async_queue)) {
             msg = g_async_queue_try_pop(input_data->midi_async_queue);
             if (msg != NULL) {
                 handle_midi_buffer(msg->buf, msg->count);
                 free_midi_message(msg);
             }
         }
-        while (g_async_queue_length_unlocked(input_data->error_async_queue)) {
+        while (g_async_queue_length(input_data->error_async_queue)) {
             err_msg = g_async_queue_try_pop(input_data->midi_async_queue);
             if (err_msg != NULL) free_error_message(err_msg);
         }
+}
+
+static int usage(char *exe) {
+    fprintf(stderr, "Usage: %s [options]\n"
+            "Options:\n"
+            "  [--backend dummy|alsa|pulseaudio|jack|coreaudio|wasapi]\n"
+            "  [--device id]\n"
+            "  [--raw]\n"
+            "  [--name stream_name]\n"
+            "  [--latency seconds]\n"
+            "  [--sample-rate hz]\n"
+            , exe);
+    return 1;
 }
 
 static void write_callback(struct SoundIoOutStream *outstream, int frame_count_min, int frame_count_max) {
@@ -80,9 +102,6 @@ static void write_callback(struct SoundIoOutStream *outstream, int frame_count_m
     float step = 1.0f / (float) sample_rate;
 
 
-
-    if (r < UINT_MAX) r++;
-    else r = 0;
 
     // Clear the buffer
     clear_buffer(frame_count_max, buffer_out);
@@ -142,6 +161,11 @@ static void write_callback(struct SoundIoOutStream *outstream, int frame_count_m
     free(buffer_out);
 }
 
+static void underflow_callback(struct SoundIoOutStream *outstream) {
+    static int count = 0;
+    fprintf(stderr, "underflow %d\n", count++);
+}
+
 int main(int argc, char **argv) {
     // Allocate a MIDI_in_data instance, assign a
     // MIDI message queue and an error queue
@@ -162,29 +186,93 @@ int main(int argc, char **argv) {
 
 
 
-    int err;
+
+    char *exe = argv[0];
+    enum SoundIoBackend backend = SoundIoBackendNone;
+    char *device_id = NULL;
+    bool raw = false;
+    char *stream_name = NULL;
+    double latency = 0.0;
+    int sample_rate = 0;
+    for (int i = 1; i < argc; i += 1) {
+        char *arg = argv[i];
+        if (arg[0] == '-' && arg[1] == '-') {
+            if (strcmp(arg, "--raw") == 0) {
+                raw = true;
+            } else {
+                i += 1;
+                if (i >= argc) {
+                    return usage(exe);
+                } else if (strcmp(arg, "--backend") == 0) {
+                    if (strcmp(argv[i], "dummy") == 0) {
+                        backend = SoundIoBackendDummy;
+                    } else if (strcmp(argv[i], "alsa") == 0) {
+                        backend = SoundIoBackendAlsa;
+                    } else if (strcmp(argv[i], "pulseaudio") == 0) {
+                        backend = SoundIoBackendPulseAudio;
+                    } else if (strcmp(argv[i], "jack") == 0) {
+                        backend = SoundIoBackendJack;
+                    } else {
+                        fprintf(stderr, "Invalid backend: %s\n", argv[i]);
+                        return 1;
+                    }
+                } else if (strcmp(arg, "--device") == 0) {
+                    device_id = argv[i];
+                } else if (strcmp(arg, "--name") == 0) {
+                    stream_name = argv[i];
+                } else if (strcmp(arg, "--latency") == 0) {
+                    latency = atof(argv[i]);
+                } else if (strcmp(arg, "--sample-rate") == 0) {
+                    sample_rate = atoi(argv[i]);
+                } else {
+                    return usage(exe);
+                }
+            }
+        } else {
+            return usage(exe);
+        }
+    }
 
     struct SoundIo *soundio = soundio_create();
-
     if (!soundio) {
         fprintf(stderr, "out of memory\n");
         return 1;
     }
 
-    if ((err = soundio_connect(soundio))) {
-        fprintf(stderr, "error connecting: %s\n", soundio_strerror(err));
+    int err = (backend == SoundIoBackendNone) ?
+        soundio_connect(soundio) : soundio_connect_backend(soundio, backend);
+
+    if (err) {
+        fprintf(stderr, "Unable to connect to backend: %s\n", soundio_strerror(err));
         return 1;
     }
+
+    fprintf(stderr, "Backend: %s\n", soundio_backend_name(soundio->current_backend));
 
     soundio_flush_events(soundio);
 
-    int default_out_device_index = soundio_default_output_device_index(soundio);
-    if (default_out_device_index < 0) {
-        fprintf(stderr, "no output device found\n");
+    int selected_device_index = -1;
+    if (device_id) {
+        int device_count = soundio_output_device_count(soundio);
+        for (int i = 0; i < device_count; i += 1) {
+            struct SoundIoDevice *device = soundio_get_output_device(soundio, i);
+            bool select_this_one = strcmp(device->id, device_id) == 0 && device->is_raw == raw;
+            soundio_device_unref(device);
+            if (select_this_one) {
+                selected_device_index = i;
+                break;
+            }
+        }
+    } else {
+        selected_device_index = soundio_default_output_device_index(soundio);
+    }
+
+    if (selected_device_index < 0) {
+        fprintf(stderr, "Output device not found\n");
         return 1;
     }
 
-    struct SoundIoDevice *device = soundio_get_output_device(soundio, default_out_device_index);
+    struct SoundIoDevice *device = soundio_get_output_device(soundio, selected_device_index);
     if (!device) {
         fprintf(stderr, "out of memory\n");
         return 1;
@@ -192,18 +280,47 @@ int main(int argc, char **argv) {
 
     fprintf(stderr, "Output device: %s\n", device->name);
 
+    if (device->probe_error) {
+        fprintf(stderr, "Cannot probe device: %s\n", soundio_strerror(device->probe_error));
+        return 1;
+    }
+
     struct SoundIoOutStream *outstream = soundio_outstream_create(device);
     if (!outstream) {
         fprintf(stderr, "out of memory\n");
         return 1;
     }
-    outstream->format = SoundIoFormatFloat32NE;
+
     outstream->write_callback = write_callback;
+    outstream->underflow_callback = underflow_callback;
+    outstream->name = stream_name;
+    outstream->software_latency = latency;
+    outstream->sample_rate = sample_rate;
+
+    if (soundio_device_supports_format(device, SoundIoFormatFloat32NE)) {
+        outstream->format = SoundIoFormatFloat32NE;
+        write_sample = write_sample_float32ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatFloat64NE)) {
+        outstream->format = SoundIoFormatFloat64NE;
+        write_sample = write_sample_float64ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatS32NE)) {
+        outstream->format = SoundIoFormatS32NE;
+        write_sample = write_sample_s32ne;
+    } else if (soundio_device_supports_format(device, SoundIoFormatS16NE)) {
+        outstream->format = SoundIoFormatS16NE;
+        write_sample = write_sample_s16ne;
+    } else {
+        fprintf(stderr, "No suitable device format available.\n");
+        return 1;
+    }
 
     if ((err = soundio_outstream_open(outstream))) {
         fprintf(stderr, "unable to open device: %s", soundio_strerror(err));
         return 1;
     }
+
+    fprintf(stderr, "Software latency: %f\n", outstream->software_latency);
+    fprintf(stderr, "'q\\n' - quit\n");
 
     if (outstream->layout_error)
         fprintf(stderr, "unable to set channel layout: %s\n", soundio_strerror(outstream->layout_error));
@@ -213,12 +330,27 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (;;)
-        soundio_wait_events(soundio);
+    for (;;) {
+        soundio_flush_events(soundio);
+        int c = getc(stdin);
+        if (c == 'q') {
+            break;
+        } else if (c == '\r' || c == '\n') {
+            // ignore
+        } else {
+            fprintf(stderr, "Unrecognized command: %c\n", c);
+        }
+    }
 
     soundio_outstream_destroy(outstream);
     soundio_device_unref(device);
     soundio_destroy(soundio);
+
+
+
+
+
+
 
     free_voice_memory();
 
@@ -229,6 +361,8 @@ int main(int argc, char **argv) {
 
     // Destroy a port configuration
     destroy_port_config(port_config);
+
+
 
     return 0;
 }
